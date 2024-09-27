0c67b6d86d1ebbdfc1980c624ec6e3258ca9ceac
VÍCTOR JOSÉ MORENILLA FUENTES <94637525+victormorenilla@users.noreply.github.com>
2024-09-27 21:10:50 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
 const connectionUrl = 'mysql://root:pjIJeIqjdTkiOTPhdeLJMdzdLTTfOctD@junction.proxy.rlwy.net:50532/railway';
 const db = mysql.createConnection(connectionUrl);
 
