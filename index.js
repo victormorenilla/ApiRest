41723eed95c0ce946d30f09284fc9928b1d929a8
VÍCTOR JOSÉ MORENILLA FUENTES <94637525+victormorenilla@users.noreply.github.com>
2024-09-27 19:04:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Configuración de la conexión a la base de datos MySQL
 const connectionUrl = 'mysql://root:pjIJeIqjdTkiOTPhdeLJMdzdLTTfOctD@junction.proxy.rlwy.net:50532/railway';
